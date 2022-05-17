f9c97d51d6bea8a68fd906e9c1ad984430766ac7
Drew Spencer <slugmandrew@gmail.com>
2022-05-17 06:37:20 -0700

    Fixing add function

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,8 +11,8 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    // comment
-    'add':      function(a,b) { return a + b },
+    // These are meant to be here
+    'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
